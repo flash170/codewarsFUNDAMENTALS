ff73843f617453c061533f98e90a136c358fe08c
flash170 <58254037+flash170@users.noreply.github.com>
2022-08-17 07:39:48 +0300

    add array with out func arr

--- SumArrays.py
+++ SumArrays.py
@@ -1,6 +1,9 @@
-def main():
-    pass
+# Initialize array
+arr = [1, 2, 3, 4, 5];
+sum = 0;
 
+# Loop through the array to calculate sum of elements
+for i in range(0, len(arr)):
+    sum = sum + arr[i];
 
-if __name__ == '__main__':
-    main()
+print("Sum of all the elements of an array: " + str(sum));
