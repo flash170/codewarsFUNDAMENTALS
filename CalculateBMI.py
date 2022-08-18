bec411912e082d76ce7a8651fe4c51d2c6f8eddb
flash170 <58254037+flash170@users.noreply.github.com>
2022-08-18 13:24:16 +0300

    You have passed all of the tests! :) CalculateBMI

--- CalculateBMI.py
+++ CalculateBMI.py
@@ -12,10 +12,11 @@ if bmi > 30 return "Obese"
 
 """def bmi(weight, height):
     i = weight/(height**2)"""
+# with out function
 
-weight = 50
+"""weight = 50
 height = 1.67
-bmi = weight/(height**2)
+bmi = round(weight/(height**2), 1)
 print(bmi)
 
 if bmi <= 18.5:
@@ -28,5 +29,45 @@ elif bmi > 30.0:
         print("Obese")
 else:
     print("no data")
+"""
 
+"""def bmi(weight, height):
+    bmii = round(weight / (height ** 2), 1)
+    if bmii <= 18.5:
+        print("Underweight")
+    elif bmii <= 25.0:
+        print("Normal")
+    elif bmii <= 30.0:
+        print("Overweight")
+    elif bmii > 30.0:
+        print("Obese")
+    else:
+        print("NoutTTT")
+    return bmii
+
+
+
+
+print(bmi(50, 1.80))"""
+
+# Correct  resuly
+"""
+Test Results:
+Log
+Normal
+Fixed Tests
+Basic Test Cases
+ (5 of 5 Assertions)
+Completed in 0.06ms
+You have passed all of the tests! :)
+"""
+def bmi(weight, height):
+    bmi = weight / height ** 2
+    if bmi <= 18.5 : return "Underweight"
+    elif bmi <= 25 : return "Normal"
+    elif bmi <= 30 : return "Overweight"
+    else: return "Obese"
+
+
+print(bmi(70, 1.7))
 
