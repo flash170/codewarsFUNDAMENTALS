a0db7aa311c0f718156872142be4918c145f93e0
flash170 <58254037+flash170@users.noreply.github.com>
2022-08-18 10:45:44 +0300

    realize bmi with out function

--- CalculateBMI.py
+++ CalculateBMI.py
@@ -9,3 +9,24 @@ if bmi <= 30.0 return "Overweight"
 
 if bmi > 30 return "Obese"
 """
+
+"""def bmi(weight, height):
+    i = weight/(height**2)"""
+
+weight = 50
+height = 1.67
+bmi = weight/(height**2)
+print(bmi)
+
+if bmi <= 18.5:
+    print("Underweight")
+elif bmi <= 25.0:
+        print("Normal")
+elif bmi <= 30.0:
+        print("Overweight")
+elif bmi > 30.0:
+        print("Obese")
+else:
+    print("no data")
+
+
