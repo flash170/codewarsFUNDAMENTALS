c62bb66219d9a2358bf18aab61fb8b7d8dda4c16
flash170 <58254037+flash170@users.noreply.github.com>
2022-08-26 10:47:07 +0300

    Test Results: Counting sheep... Test Passed You have passed all of the tests! :)

--- Countingsheep....py
+++ Countingsheep....py
@@ -1,5 +1,6 @@
 """
-Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the number of sheep present in the array (true means present).
+Consider an array/list of sheep where some sheep may be missing from their place. We need a function that counts the
+number  of sheep present in the array (true means present).
 
 For example,
 
@@ -13,3 +14,20 @@ The correct answer would be 17.
 
 Hint: Don't forget to check for bad values like null/undefined
 """
+
+"""sheep = [False,  True,  True,  False]
+print(sheep.count(True))"""
+# print("True in {}".format(sheep.index(True)))
+"""
+for i in range(0, len(sheep)):
+    if sheep[i]:
+        m = sheep[i]
+        print(m)
+        """
+
+
+def count_sheeps(sheep):
+    return sheep.count(True)
+
+
+print(count_sheeps([False, True, True, False]))
