aa604b087b8b6db1fab459d1b53685c91d489adf
flash170 <58254037+flash170@users.noreply.github.com>
2022-09-01 09:49:27 +0300

    Test Results: Mumbling accum Basic tests  (5 of 5 Assertions) You have passed all of the tests! :)

--- Mumbling.py
+++ Mumbling.py
@@ -7,3 +7,13 @@ accum("RqaEzty") -> "R-Qq-Aaa-Eeee-Zzzzz-Tttttt-Yyyyyyy"
 accum("cwAt") -> "C-Ww-Aaa-Tttt"
 The parameter of accum is a string which includes only letters from a..z and A..Z.
 """
+
+def accum(s):
+    output = []
+    for count, letter in enumerate(s):
+        output.append(letter.upper() + letter.lower() * count)
+    return '-'.join(output)
+
+
+print(accum("abcd"))
+print(accum("RqaEzty"))
